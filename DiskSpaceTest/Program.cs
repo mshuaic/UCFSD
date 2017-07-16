076a8f5f501e50c5eed72a8bdb1595c2bf95955b
Mark <mshuaic@knights.ucf.edu>
2017-07-16 04:20:07 -0400

    Test case generator uses DataExraction tool now

--- DiskSpaceTest/Program.cs
+++ DiskSpaceTest/Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Xml;
 using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using CheckupExec;
+using CheckupExec.Models;
+
 
 namespace DiskSpaceTest
 {
@@ -9,27 +14,30 @@ namespace DiskSpaceTest
         static string Output = Path.Combine(Directory.GetCurrentDirectory(), "DiskSpaceTest.xml");
 
         static int Max = 30;
-
-        static int NumOfDisk = 1;
-
-        static int InitTotalSpace = 1024 * 10 * MB2B;
-
-        static int InitUsedSpace = (int)(InitTotalSpace * 0.2); 
         
-        static int GrowthRange = 512 * MB2B;
+        static int GrowthRange = 1024 * MB2B;
 
         const int MB2B = 1024;
         static void Main(string[] args)
         {
-            if(args.Length != 0)
+            if(args.Length == 1)
+            {
+                GrowthRange = int.Parse(args[0]) * MB2B;
+            }
+
+            if(args.Length == 3)
             {
                 Output = args[0];
                 Max = int.Parse(args[1]);
-                NumOfDisk = int.Parse(args[2]);
-                InitTotalSpace = int.Parse(args[3]) * MB2B;
-                InitUsedSpace = int.Parse(args[4]) * MB2B;
-                GrowthRange = int.Parse(args[5]) * MB2B;
+                GrowthRange = int.Parse(args[2]) * MB2B;
             }
+            
+            // remote
+            //var de = new BEStorage(true, "mshuaic", "VM", "mshuaic");
+
+            // local
+             var de = new BEStorage(false);
+            List<Storage> storages =  de.GetStorages();
 
             using (XmlWriter writer = XmlWriter.Create(Output))
             {
@@ -37,15 +45,29 @@ namespace DiskSpaceTest
                 writer.WriteStartElement("DiskCapacities");
                 writer.WriteAttributeString("count", Max.ToString());
                 writer.WriteAttributeString("ServerName", System.Environment.MachineName);
-                int usedSpace = InitUsedSpace;
                 DateTime date = DateTime.Now;
-                Random rnd = new Random();   
+                Random rnd = new Random();
+                List<long> usedSpace = storages.Select(x => x.UsedCapacityBytes).ToList();
+
                 for (int i=0;i<Max;i++)
                 {
-                    logDirveInfo(writer,usedSpace,date);
-                    usedSpace += rnd.Next(0, GrowthRange);
+                    
+                    writer.WriteStartElement("DiskCapacityInstance");
+                    writer.WriteAttributeString("Date", date.ToString("yyyy-MM-dd hh:mm:sszzz"));
+                    for(int j=0;j<storages.Count;j++)
+                    {
+                        writer.WriteStartElement("Disk");
+                        writer.WriteAttributeString("Name", storages[j].Name);
+                        writer.WriteElementString("TotalCapacityBytes", storages[j].TotalCapacityBytes.ToString());
+                        writer.WriteElementString("UsedCapacityBytes", usedSpace[j].ToString());
+                        writer.WriteEndElement();
+                        if (storages[j].Name == "Any disk storage")
+                            continue;
+                        usedSpace[j] += rnd.Next(0, GrowthRange);
+                    }
+                    writer.WriteEndElement();
                     date = date.AddDays(1);
-                }               
+                }
                 writer.WriteEndElement();
                 writer.WriteEndDocument();
                 writer.Close();
@@ -53,20 +75,18 @@ namespace DiskSpaceTest
 
         }
 
-        static private void logDirveInfo(XmlWriter writer, int usedSpace, DateTime date)
+        private class BEStorage : DataExtraction
         {
-            writer.WriteStartElement("DiskCapacityInstance");
-            writer.WriteAttributeString("Date", date.ToString("yyyy-MM-dd hh:mm:sszzz"));
+            public BEStorage(bool isRemoteUser, string password = null, string serverName = null, string userName = null)
+            : base(isRemoteUser, password, serverName, userName)
+            { }
 
-            for(int i=1;i<=NumOfDisk;i++)
+            public List<Storage> GetStorages()
             {
-                writer.WriteStartElement("Disk");
-                writer.WriteAttributeString("Name", String.Format("Disk storage {0:D4}", i));
-                writer.WriteElementString("TotalCapacityBytes", InitTotalSpace.ToString());
-                writer.WriteElementString("UsedCapacityBytes", usedSpace.ToString());
-                writer.WriteEndElement();
+                return StorageController.GetStorages();
             }
-            writer.WriteEndElement();
         }
     }
+
+
 }
