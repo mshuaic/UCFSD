1a4c571684a798472d9e125b2a7ef4424c91bafc
Dan Lott <danmanlott@gmail.com>
2017-07-20 15:16:50 -0400

    Warnings, separate templates, Fixed Job Report

--- CheckupExec/Utilities/HtmlGen.cs
+++ CheckupExec/Utilities/HtmlGen.cs
@@ -13,56 +13,37 @@ namespace CheckupExec.Utilities
         public void readTemplate(string path)
         {
             string html = File.ReadAllText(path);
-
         }
 
         public void makeReportFore(List<FrontEndCapacityReport> reps, string loc)
         {
-            //dir = "B:/report.html";
             string html = File.ReadAllText("templateFrontEnd.html");
-
             foreach(FrontEndCapacityReport sinRep in reps){
 
             }
-            //double space = sinRep.HistoricalPoints[HistoricalPoints.Count - 1].GB;
-            //int free = space - 100;
-            //Driver d = new Driver("C", space, (space-100), 100);
             SectionGen sc = new SectionGen();
             string newHtml = html.Insert(html.IndexOf("<!--START REPORT-->") + "<!--START REPORT-->".Length, sc.middleFore(reps));
             newHtml = newHtml.Insert(newHtml.IndexOf("<!--CHARTS AREA-->") + "<!--CHARTS AREA-->".Length, sc.chartFore(reps));
             System.IO.File.WriteAllText(loc,newHtml);
-
-
-            //Console.WriteLine(newHtml);
-
             Console.ReadKey();
 
         }
         public void makeReportDisk(List<DiskCapacityReport> reps, string loc)
         {
-
-            //dir = "B:/report.html";
-            string html = File.ReadAllText("template.html");
-            //double space = points[points.Count - 1].GB;
-            //int free = space - 100;
-            //Driver d = new Driver("C", space, (space-100), 100);
+            string html = File.ReadAllText("templateDisk.html");
             SectionGen sc = new SectionGen();
             string newHtml = html.Insert(html.IndexOf("<!--START REPORT-->") + "<!--START REPORT-->".Length, sc.middleDisk(reps));
             newHtml = newHtml.Insert(newHtml.IndexOf("<!--CHARTS AREA-->") + "<!--CHARTS AREA-->".Length, sc.chartDisk(reps));
             System.IO.File.WriteAllText(loc, newHtml);
-
-
-            //Console.WriteLine(newHtml);
-
             Console.ReadKey();
 
         }
         public void makeReportJob(List<BackupJobReport> reps, string loc) {
 
-            string html = File.ReadAllText("template.html");
+            string html = File.ReadAllText("templateJob.html");
             SectionGen sc = new SectionGen();
-            string newHtml = html.Insert(html.IndexOf("<!--START REPORT-->") + "<!--START REPORT-->".Length, sc.middleDisk(reps));
-            newHtml = newHtml.Insert(newHtml.IndexOf("<!--CHARTS AREA-->") + "<!--CHARTS AREA-->".Length, sc.chartDisk(reps));
+            string newHtml = html.Insert(html.IndexOf("<!--START REPORT-->") + "<!--START REPORT-->".Length, sc.middleJob(reps));
+            newHtml = newHtml.Insert(newHtml.IndexOf("<!--CHARTS AREA-->") + "<!--CHARTS AREA-->".Length, sc.chartJob(reps));
             System.IO.File.WriteAllText(loc, newHtml);
 
             Console.ReadKey();
--- CheckupExec/Utilities/SectionGen.cs
+++ CheckupExec/Utilities/SectionGen.cs
@@ -104,21 +104,18 @@ namespace CheckupExec.Utilities
                                 writer.RenderBeginTag(HtmlTextWriterTag.P);
                                 writer.Write("100% Capacity: " + rep.DaysToFull + " Days");
                                 writer.RenderEndTag();
-
-
-                                writer.RenderBeginTag(HtmlTextWriterTag.P);
-                                //writer.Write("Free Space: " + d.getFreePer("n2") + "%");
-                                writer.RenderEndTag();
+                                if (rep.DaysToFull < 97)
+                                {
 
                                 writer.RenderBeginTag(HtmlTextWriterTag.P);
-                                //writer.Write("Used Space: " + d.getUsedPer("n2") + "%");
+
+                                writer.Write("Warning! Examine disk storage, depletion soon");
+
                                 writer.RenderEndTag();
-                            writer.RenderEndTag();
+                                }
+                                writer.RenderEndTag();
 
                             writer.AddAttribute(HtmlTextWriterAttribute.Class, "item");
-                                writer.AddAttribute(HtmlTextWriterAttribute.Id, "pie");
-                                writer.RenderBeginTag(HtmlTextWriterTag.Div);
-                                writer.RenderEndTag();
                             writer.RenderEndTag();
 
                             writer.AddAttribute(HtmlTextWriterAttribute.Id, $"histTrace{i}");
@@ -143,12 +140,8 @@ namespace CheckupExec.Utilities
             foreach (DiskCapacityReport rep in reps)
             {
                 i++;
-                //List<PlotPoints> histPoints = rep.HistoricalPoints;
-                //List<PlotPoint> futurePoints = rep.ForecastPoints;
-
                 stringWriter.Write($"\nvar histTrace{i}");
                 stringWriter.Write(" = {x: [");
-                PlotPoint va;
 
                 foreach (PlotPoint po in rep.HistoricalPoints)
                 {
@@ -167,8 +160,6 @@ namespace CheckupExec.Utilities
 
                 stringWriter.Write($"\n\nvar futTrace{i}");
                 stringWriter.Write(" = {x: [");
-                //PlotPoint va;
-
                 foreach (PlotPoint po in rep.ForecastPoints)
                 {
                     stringWriter.Write($"'{po.Days}',");
@@ -189,45 +180,51 @@ namespace CheckupExec.Utilities
         public string middleFore(List<FrontEndCapacityReport> reps)
         {
             StringWriter stringWriter = new StringWriter();
+            int i = 0,j = 0;
             using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter))
             {
-                writer.AddAttribute(HtmlTextWriterAttribute.Style, "margin: auto; width: 80 %; ");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                foreach (FrontEndCapacityReport rep in reps)
+                {
+                    i++;
+                    writer.AddAttribute(HtmlTextWriterAttribute.Style, "margin: auto; width: 80 %; ");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "rows");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "rows");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "columns");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "columns");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "item");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "item");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
 
-                writer.RenderBeginTag(HtmlTextWriterTag.P);
-                //writer.Write("Disk " + d.Name + ": ");
-                //writer.RenderEndTag();
+                    
 
-                writer.RenderBeginTag(HtmlTextWriterTag.P);
-                //writer.Write("Gigabytes of Data Protected: " + d.getFreePer("n2") + "Gb");
-                //writer.RenderEndTag();
+                    writer.AddAttribute(HtmlTextWriterAttribute.Id, $"foreHistTrace{i}");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.RenderEndTag();
+                    j = 0;
+                    foreach (BackupJobReport bacRep in rep.BackupJobs)
+                    {
+                        j++;
+                        writer.RenderBeginTag(HtmlTextWriterTag.P);
+                        writer.Write("Job Name: " + bacRep.JobName);
+                        writer.RenderEndTag();
+                        writer.AddAttribute(HtmlTextWriterAttribute.Id, $"foreHistTrace{i}-{j}");
+                        writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                        writer.RenderEndTag();
+                    }
 
-                //writer.RenderBeginTag(HtmlTextWriterTag.P);
-                //writer.Write("Used Space: " + d.getUsedPer("n2") + "%");
-                //writer.RenderEndTag();
-                writer.RenderEndTag();
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "item");
 
-                writer.AddAttribute(HtmlTextWriterAttribute.Class, "item");
-                writer.AddAttribute(HtmlTextWriterAttribute.Id, "pie");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
-                writer.RenderEndTag();
-                writer.RenderEndTag();
+                    writer.RenderEndTag();
 
-                writer.AddAttribute(HtmlTextWriterAttribute.Id, "line");
-                writer.RenderBeginTag(HtmlTextWriterTag.Div);
-                writer.RenderEndTag();
-                writer.RenderEndTag();
-                writer.RenderEndTag();
-
+                    writer.AddAttribute(HtmlTextWriterAttribute.Id, "line");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.RenderEndTag();
+                    writer.RenderEndTag();
+                    writer.RenderEndTag();
+                }
             }
 
             return stringWriter.ToString();
@@ -235,36 +232,185 @@ namespace CheckupExec.Utilities
         public string chartFore(List<FrontEndCapacityReport> reps)
         {
             StringWriter stringWriter = new StringWriter();
-
+            int i=0,j = 0;
             foreach (FrontEndCapacityReport rep in reps)
             {
+                i++;
                 stringWriter.Write("\n var trace1 = {x: [");
-                PlotPoint va;
-
+                stringWriter.Write($"\nvar foreHistTrace{i}");
+                stringWriter.Write(" = {x: [");
 
                 foreach (PlotPoint po in rep.HistoricalPoints)
                 {
                     stringWriter.Write($"'{po.Days}',");
                 }
-                stringWriter.Write("],y: [");
+                stringWriter.Write("], \ny: [");
                 foreach (PlotPoint po in rep.HistoricalPoints)
                 {
                     stringWriter.Write($"{po.GB},");
                 }
-                stringWriter.Write("],mode: 'lines+markers',};var data = [trace1];var layout = {autosize: true,title: 'Amount of Data under Protection',height:400,width:400,xaxis:{title: 'Days',},yaxis:{title: 'Gigabytes of Data Protected'}};");
-                stringWriter.Write($"Plotly.newPlot('line', data, layout);");
+                stringWriter.Write("]\n,mode: 'lines+markers',marker:{color: 'rgb(179,4,16)'}};var dataForeHist");
+                stringWriter.Write($"{i}= [histTrace{i}];\nvar layout = ");
+                stringWriter.Write("{autosize: true,title: 'Data being protected',height:400,width:400,\n xaxis:{title: 'Days',},yaxis:{title: 'Data(GB)'}};");
+                stringWriter.Write($"\nPlotly.newPlot('foreHistTrace{i}', dataForeHist{i}, layout);");
+                j = 1;
+                foreach (BackupJobReport bacRep in rep.BackupJobs)
+                {
+
+                    stringWriter.Write("\n var trace1 = {x: [");
+                    stringWriter.Write($"\nvar foreHistTrace{i}-{j}");
+                    stringWriter.Write(" = {x: [");
+
+                    foreach (PlotPoint po in rep.HistoricalPoints)
+                    {
+                        stringWriter.Write($"'{po.Days}',");
+                    }
+                    stringWriter.Write("], \ny: [");
+                    foreach (PlotPoint po in rep.HistoricalPoints)
+                    {
+                        stringWriter.Write($"{po.GB},");
+                    }
+                    stringWriter.Write("]\n,mode: 'lines+markers',marker:{color: 'rgb(179,4,16)'}};var dataForeHist");
+                    stringWriter.Write($"{i}-{j}= [histTrace{i}-{j}];\nvar layout = ");
+                    stringWriter.Write("{autosize: true,title: 'Data being protected',height:400,width:400,\n xaxis:{title: 'Days',},yaxis:{title: 'Data(GB)'}};");
+                    stringWriter.Write($"\nPlotly.newPlot('foreHistTrace{i}-{j}', dataForeHist{i}-{j}, layout);");
+                    j++;
+                }
+
             }
             return stringWriter.ToString();
         }
 
         public string middleJob(List<BackupJobReport> reps) {
+            StringWriter stringWriter = new StringWriter();
+            using (HtmlTextWriter writer = new HtmlTextWriter(stringWriter))
+            {
+                int i = 0;
+                foreach (BackupJobReport rep in reps)
+                {
+                    i++;
+                    
 
-            return "";
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "rows");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "columns");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                    writer.AddAttribute(HtmlTextWriterAttribute.Class, "item");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("Diskname: " + rep.StorageName);
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("Diskname: " + rep.StorageName);
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("Diskname: " + rep.StorageType);
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("Maximum Capacity: " + rep.MaxCapacity + "GB");
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("Used Capacity: " + rep.UsedCapacity + "GB");
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("50% Capacity: " + rep.DaysTo50 + " Days");
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("75% Capacity: " + rep.DaysTo75 + " Days");
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("90% Capacity: " + rep.DaysTo90 + " Days");
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.Write("100% Capacity: " + rep.DaysToFull + " Days");
+                    writer.RenderEndTag();
+                   
+
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.RenderEndTag();
+
+                    writer.RenderBeginTag(HtmlTextWriterTag.P);
+                    writer.RenderEndTag();
+                    writer.RenderEndTag();
+
+                   
+                    writer.RenderEndTag();
+
+                    writer.AddAttribute(HtmlTextWriterAttribute.Id, $"histTrace{i}");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.RenderEndTag();
+                    writer.AddAttribute(HtmlTextWriterAttribute.Id, $"futTrace{i}");
+                    writer.RenderBeginTag(HtmlTextWriterTag.Div);
+                    writer.RenderEndTag();
+
+                    writer.RenderEndTag();
+                    
+                }
+
+            }
+
+            return stringWriter.ToString();
         }
         public string chartJob(List<BackupJobReport> reps)
         {
+            StringWriter stringWriter = new StringWriter();
+            int i = 0;
+            foreach (BackupJobReport rep in reps)
+            {
+                i++;
 
-            return "";
+                stringWriter.Write($"\nvar histTrace{i}");
+                stringWriter.Write(" = {x: [");
+
+                foreach (PlotPoint po in rep.HistoricalPoints)
+                {
+                    stringWriter.Write($"'{po.Days}',");
+                }
+                stringWriter.Write("], \ny: [");
+                foreach (PlotPoint po in rep.HistoricalPoints)
+                {
+                    stringWriter.Write($"{po.GB},");
+                }
+                stringWriter.Write("]\n,mode: 'lines+markers',marker:{color: 'rgb(179,4,16)'}};var dataHist");
+                stringWriter.Write($"{i}= [histTrace{i}];\nvar layout = ");
+                stringWriter.Write("{autosize: true," +
+                    "title: 'Job Size for the past 30 days',height:400,width:400,\n " +
+                    "xaxis:{title: 'Days',}," +
+                    "yaxis:{title: 'Size(GB)'}};");
+                stringWriter.Write($"\nPlotly.newPlot('histTrace{i}', dataHist{i}, layout);");
+
+                stringWriter.Write($"\n\nvar futTrace{i}");
+                stringWriter.Write(" = {x: [");
+                foreach (PlotPoint po in rep.ForecastPoints)
+                {
+                    stringWriter.Write($"'{po.Days}',");
+                }
+                stringWriter.Write("], \ny: [");
+                foreach (PlotPoint po in rep.ForecastPoints)
+                {
+                    stringWriter.Write($"{po.GB},");
+                }
+                stringWriter.Write("]\n,mode: 'lines+markers',marker:{color: 'rgb(179,4,16)'}};var dataFut");
+                stringWriter.Write($"{i}= [futTrace{i}];\nvar layout = ");
+                stringWriter.Write("{autosize: true," +
+                    "title: 'Projected Job Size for the next 30 days',height:400,width:400,\n " +
+                    "xaxis:{title: 'Days',}," +
+                    "yaxis:{title: 'Size(GB)'}};");
+                stringWriter.Write($"\nPlotly.newPlot('futTrace{i}', dataFut{i}, layout);");
+            }
+            return stringWriter.ToString();
         }
 
     }
